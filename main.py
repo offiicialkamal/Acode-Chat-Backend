2924a2bcf8c889fdaba54041c131717da5060909
Kamal Yadav <indianhelper@gmail.com>
2025-07-25 12:59:39 +0530

    added multiple routs and functions all will be updated in next commit

--- main.py
+++ main.py
@@ -1,60 +1,113 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 import random
 
 app=Flask("__name__")
+def provide_new_id():
+    new_id = str(random.randint(100000000, 9999999999))
+    # now check this id is not given to any user before
+    # first get the database keys  list and thn check using in keyword
+    old_database = provide_database()
+    if new_id not in list(old_database.keys()):
+        return new_id
+    else:
+        print("id is already avilable findng again")
+        provide_new_id()
 
 def provide_database():
     with open("users_database.json", "r") as f:   
-        # DATBASE_JSON  = f.read()
         return json.load(f)
 
-def write_in_database(new_users_data):
-    try:
-        with open("users_database.json", "w") as f:
-            json.dump(new_users_data, f, indent=4)
-            return True
-    except Exception as e:
-        print(f"error while writing new user in database {new_users_data} \n \n \n {e}")
-        return False
-
-def update_database(data_to_update):
+def write_in_database(data_to_update):
+    old_database = provide_database()
     with open("users_database.json", "w") as f:
-        f.update(data_to_update)
+        # old_database[list(data_to_update.keys())[0]] = data_to_update
+        old_database[data_to_update.get("id")] = data_to_update
+        json.dump(old_database, f, indent=4)        
+
+def update_database(userID, key, value):
+    old_database = provide_database()
+    with open("users_database.json", "w") as f:
+        old_database.get(userID)[key] = value
+        json.dump(old_database, f, indent=4)
         
-    
-def provide_new_id():
-    new_id = str(random.randint(100000000, 9999999999))
-    return new_id
 
+def generate_cookie_string():
+    return "fbabasd iasddibasdhbhasdib saasdlbasdib asdciasdib asdiliibbsdliu "
 
-def create_new_account(name, email, number, password, location):
+def generate_new_accessToken():
+    return "oduauyyasgasygdyasiuasasdliasdi"
+
+def get_date_time_now():
+    return "its date time now"
+
+def create_new_account(name, email, number, DOB, password, location, dateTime):
     data = {
-        provide_new_id():{
-                    "name": name,
-                    "email": email,
-                    "number": number,
-                    "password": password,
-                    "location": location,
-                    "cookie": generate_cookie_string(),
-                    "accessToken": generate_new_accessToken(),
-                    "lastLogin": dateTime,
-                    "accountCreationDateTime": get_date_time_now()
+        "id": provide_new_id(),
+        "name": name,
+        "email": email,
+        "number": number,
+        "DOB": DOB,
+        "password": password,
+        "location": location,
+        "cookie": generate_cookie_string(),
+        "accessToken": generate_new_accessToken(),
+        "lastLogin": dateTime,
+        "last_pw_change": dateTime,
+        "accountCreationDateTime": dateTime
         }
-    }
+    
+    write_in_database(data)
 
-
-
-
-
-
-# print(provide_database())
-# print(type(provide_database()))
-# print(type(num1 := provide_new_id()))
-# print(num1)
 
 @app.route("/login", methods=["POST"])
 def login():
     if request.method == "POST":
         request.form.get("email")
         request.form.get("password")
+
+@app.route("/create_new_account", methods=["GET", "POST"])
+def create_acc():
+    #collect all data and pass on write data function
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    location = request.form.get("ip_info")
+    dateTime = request.form.get("dateTime")
+    number = request.form.get("number") if request.form.get("number") else "not provided"
+    DOB = request.form.get("DOB") if request.form.get("DOB") else "not provided"
+    ## now write all a=dat to the program
+    create_new_account(name, email, number, DOB, password, location, dateTime)
+
+@app.route("/reset_password", methods=["GET", "POST"])
+def reset_pw_home():
+    global sended_otp
+    # check the otp and reste the password on request
+    if request.method == "POST":        #using post method because of its a secure method
+        userID = request.form.get("userID")
+        new_password = request.form.get("new_password")
+        enterd_otp = request.form.get("enterd_otp")
+        last_pw_change = request.form.get("todays_date")
+        if userID in list(provide_database.keys()) and enterd_otp == sended_otp:
+            ## sucessfully user verified
+            update_database(
+                userID,
+                "password",
+                new_password
+            )
+            update_database(
+                userID,
+                "last_pw_change",
+                last_pw_change
+            )
+        else:
+            print("wrong otp provided")
+            return jsonify({"message": "wrong otp"}), 401
+    elif request.method == "GET":
+        return render_template("otp.html")
+    else:
+        return jsomify({"message": "Method not allowed"}), 401
+    
+
+
+#ill handle all request =s in next commit
